8f325a766447b504c48c25bb86a0516a42ba68f2
Tony Adams <tadams@lappy386.(none)>
2012-10-07 22:49:35 -0500

    OO'ed the code a little more.  Still has some work to go before working on the UI

--- stratMaker.py
+++ stratMaker.py
@@ -11,39 +11,41 @@ class Line():
     start = (0,0)
     end = (0,0)
 
-    def __init__(self, start, end):
+    def __init__(self, start, end, color):
         self.start = start
         self.end =end
+        self.color = color
+
+    def draw(self, screen):
+        pygame.draw.line(screen, self.color, self.start, self.end)
+
+class Mode():
+    edit = 0
+    playback = 1
 
 class Player():
     def __init__(self, color, rect, name):
         self.color = color
         self.rect = rect
+        self.size = self.rect.size
         self.name = name
         self.drag = False
         self.lines = []
+    
+    def draw(self, screen, timeStamp=TimeStamp(-1),mode=Mode.edit):
+        # if Editting, only draw the last rect
+        if Mode.edit == mode:
+            pygame.draw.rect( screen, self.color, self.rect)
 
-def drawEditMode(screen, list_of_players):
-    for player in list_of_players:
-        pygame.draw.rect( screen, player.color, player.rect)
-
-        for lineDict in player.lines:
-            for lineList in lineDict.itervalues():
-                for line in lineList:
-                        pygame.draw.line(screen, player.color, line.start, line.end)
-
-def drawPlaybackMode(screen, list_of_players, currentTime):
-    for player in list_of_players:
-        for lineDict in player.lines:
-            times = range(0, currentTime.time+1)
-            print times
-            for time in times:
-                if lineDict.has_key(time):
-                    print lineDict
-                    for line in lineDict[time]: 
-                        pygame.draw.line(screen, player.color, line.start, line.end)
-                    pygame.draw.rect( screen, player.color, Rect(line.end, (10,10)))
-
+        #for either draw the lines, but if playback play to the current time
+        for lineDict in self.lines:
+            for time,lines in lineDict.iteritems():
+                if Mode.playback == mode and  time <= timeStamp.time or Mode.edit == mode:
+                    for line in lines:
+                        line.draw(screen)
+                    if Mode.playback == mode:
+                        pygame.draw.rect(screen, self.color, Rect(line.end, player.size))
+                    
 pygame.init()
 
 size = (600,400)
@@ -83,15 +85,17 @@ while True:
                 if player.rect.collidepoint(event.pos):
                     player.drag = True
 
+                    # Create function to add lines
+
                     if player.lines:
                         if player.lines[-1].has_key(currentTime.time):
                             #set lines here
-                            player.lines[-1][currentTime.time].append(Line(player.rect.center, player.rect.center))
+                            player.lines[-1][currentTime.time].append(Line(player.rect.center, player.rect.center, player.color))
                         else:
                             #create the dict and list
-                            player.lines[-1][currentTime.time] = [Line(player.rect.center, player.rect.center)]
+                            player.lines[-1][currentTime.time] = [Line(player.rect.center, player.rect.center, player.color)]
                     else:
-                        player.lines.append( {currentTime.time : [Line(player.rect.center, player.rect.center)] } )
+                        player.lines.append( {currentTime.time : [Line(player.rect.center, player.rect.center, player.color)] } )
         elif MOUSEBUTTONUP == event.type:
             for player in list_of_players:
                 if player.drag:
@@ -100,28 +104,28 @@ while True:
         #KEYBOARD INPUT
         # + : Advance curTime
         # - : Revert curTime
+        # m : switch mode
         elif KEYDOWN == event.type:
+            # Adj. Time
             if event.unicode == u'+':
                 currentTime.time = currentTime.time + 1
             elif event.unicode == u'-':
                 currentTime.time = currentTime.time - 1
                 if currentTime.time < 0:
                     currentTime.time = 0
+            # Set Mode
             elif event.unicode == u'm':
-                if mode == 0:
-                    mode = 1
+                if mode == Mode.edit:
+                    mode = Mode.playback
                 else:
-                    mode = 0
+                    mode = Mode.edit
             print currentTime.time
 
 
 
 
         screen.blit(map, (0,0))
-        if mode == 0:
-            drawEditMode(screen, list_of_players)
-        elif mode == 1:
-            drawPlaybackMode(screen, list_of_players, currentTime)
-            
+        for player in list_of_players:
+            player.draw(screen, currentTime, mode)
 
         pygame.display.flip() 
